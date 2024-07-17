6839fce14684be42068024e55ac0e919c7770b97
sulumor <romuald.patfoort@gmail.com>
2024-07-17 16:03:01 +0200

    feat : init helmet

--- back/app/index.app.js
+++ back/app/index.app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import cors from "cors";
+import helmet from 'helmet';
 import cookieParser from "cookie-parser";
 import router from "./routers/index.router.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
@@ -20,6 +21,7 @@ const app = express();
 createDoc(app);
 
 app.use(cors(corsOptions));
+app.use(helmet())
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
